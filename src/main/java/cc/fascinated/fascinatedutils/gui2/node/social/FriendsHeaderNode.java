d5441bce7e70e6ee436e8cfb769e20ad3a50fc8b
fascinated <liam@fascinated.cc>
2026-05-10 03:16:20 +0100

    total friends

--- src/main/java/cc/fascinated/fascinatedutils/gui2/node/social/FriendsHeaderNode.java
+++ src/main/java/cc/fascinated/fascinatedutils/gui2/node/social/FriendsHeaderNode.java
@@ -1,5 +1,6 @@
 package cc.fascinated.fascinatedutils.gui2.node.social;
 
+import cc.fascinated.fascinatedutils.api.Alumite;
 import cc.fascinated.fascinatedutils.gui2.node.TextNode;
 import cc.fascinated.fascinatedutils.gui2.theme.UiTheme;
 import net.minecraft.network.chat.Component;
@@ -7,7 +8,9 @@ import net.minecraft.network.chat.Component;
 public class FriendsHeaderNode extends SocialPanelHeaderNode {
 
     public FriendsHeaderNode() {
-        TextNode title = new TextNode(Component.translatable("alumite.social.friends_heading").getString())
+        int count = Alumite.INSTANCE != null ? Alumite.INSTANCE.users().getFriends().size() : 0;
+        String heading = Component.translatable("alumite.social.friends_heading").getString() + " (" + count + ")";
+        TextNode title = new TextNode(heading)
                 .setColorResolver(UiTheme::textPrimary)
                 .setTextAlign(0f, 0.5f);
         title.left(12).right(32).top(0).bottom(0);
